2c3f7487ceb50c59711e96be82b6c79489619e42
Ayush Kumar <ayush1031kumar@gmail.com>
2025-06-06 19:09:29 +0530

    checked for the correct redirection

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router , Routes } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar";
 import { Home,About,Projects,Contact } from "./components/pages";
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Route path="/about" element= {<About/>}/>
           <Route path="/Projects" element= {<Projects/>}/>
           <Route path="/contact" element= {<Contact/>}/>
+          <Route path="*" element={<NoMatch/>}/>
+          <Route path="/ayush-portfolio" element={<Home/>} />
         </Routes>
       </Router>
     </main>
